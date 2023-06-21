591a25795659d84bb62bcfa1fd44fbe393fbb3ad
Edwin Chen <edwinchen018@gmail.com>
2023-06-20 19:33:58 -0600

    Correccion y limpieza de codigo para el funcionamiento de header y footer

--- TiendaWeb/src/test/java/com/tienda/TiendaApplicationTests.java
+++ TiendaWeb/src/test/java/com/tienda/TiendaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.tienda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicApplicationTests {
+class TiendaApplicationTests {
 
 	@Test
 	void contextLoads() {
