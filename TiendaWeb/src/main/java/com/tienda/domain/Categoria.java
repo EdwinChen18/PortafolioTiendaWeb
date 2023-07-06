56a3614c85c2b46a565baff15b7102af524f9cf1
Edwin Chen <edwinchen018@gmail.com>
2023-07-05 18:31:46 -0600

    se agrego productos+pruebas
    semana8

--- TiendaWeb/src/main/java/com/tienda/domain/Categoria.java
+++ TiendaWeb/src/main/java/com/tienda/domain/Categoria.java
@@ -2,6 +2,7 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -19,6 +20,11 @@ public class Categoria implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
+        
+    @OneToMany
+    @JoinColumn(name="id_categoria")
+    List<Producto> productos;
+    
     public Categoria() {
     }
 
@@ -26,4 +32,5 @@ public class Categoria implements Serializable {
         this.descripcion = categoria;
         this.activo = activo;
     }
+
 }
